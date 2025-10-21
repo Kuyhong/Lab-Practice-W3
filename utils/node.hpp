0e4eebde8fe9450ba0f4c7b1fa7e634e0c956db2
Chea Kuyhong <kuyhongchea.dev@gmail.com>
2025-10-21 10:59:04 +0700

    Node.hpp modified

---
+++ utils/node.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public: 
+    int value;
+    Node* next;
+    Node* prev;
+
+    public:
+    Node (int value, Node* next, Node* prev){
+        this->value = value;
+        this->next = next;
+        this->prev = prev;
+    }
+};
